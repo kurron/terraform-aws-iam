02c3fb0ac664b8e310a0c4b35f5a2a5dd08d7458
Ron Kurr <kurr@kurron.org>
2017-10-11 21:11:13 -0400

    Full life-cycle works

--- debug/plan.tf
+++ debug/plan.tf
@@ -10,3 +10,47 @@ module "iam" {
     project     = "Debug"
     environment = "development"
 }
+
+output "default_ecs_role_id" {
+    value = "${module.iam.default_ecs_role_id}"
+}
+
+output "arn" {
+    value = "${module.iam.arn}"
+}
+
+output "profile" {
+    value = "${module.iam.profile}"
+}
+
+output "cross_account_ecr_pull_role_id" {
+    value = "${module.iam.cross_account_ecr_pull_role_id}"
+}
+
+output "cross_account_ecr_pull_role_arn" {
+    value = "${module.iam.cross_account_ecr_pull_role_arn}"
+}
+
+output "cross_account_ecr_pull_profile_id" {
+    value = "${module.iam.cross_account_ecr_pull_profile_id}"
+}
+
+output "start_stop_role_id" {
+    value = "${module.iam.start_stop_role_id}"
+}
+
+output "start_stop_role_arn" {
+    value = "${module.iam.start_stop_role_arn}"
+}
+
+output "start_stop_profile_id" {
+    value = "${module.iam.start_stop_profile_id}"
+}
+
+output "dynamic_dns_role_id" {
+    value = "${module.iam.dynamic_dns_role_id}"
+}
+
+output "dynamic_dns_role_arn" {
+    value = "${module.iam.dynamic_dns_role_arn}"
+}
